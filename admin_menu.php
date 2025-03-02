3d34a9876bd4ded7bbbab955551528c7f441a36a
TrustTheGuardian <jasonsantos1228@gmail.com>
2025-03-03 00:40:57 +0800

    Create admin_menu.php

---
+++ admin_menu.php
@@ -0,0 +1,13 @@
+<section id="menu">
+    <div class="logo">
+        <img src="" alt="Purrfectly Stitch">
+        <h2>Purrfectly Stitch</h2>
+    </div>
+    <div class="items">
+        <li><i class="bi bi-clipboard2-data-fill"></i><a href="dashboard.php"> Dashboard</a></li>
+        <li><i class="bi bi-file-earmark-text-fill"></i><a href="reports.php"> Reports</a></li>
+        <li><i class="bi bi-bag-check-fill"></i><a href="purchase_order.php"> Purchase Order</a></li>
+        <li><i class="bi bi-people-fill"></i><a href="users.php"> Users</a></li>
+        <li><i class="bi bi-box2-heart-fill"></i><a href="products.php"> Products</a></li>
+    </div>
+</section>
