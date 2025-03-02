dcf982b80ab70f7159d0a4cc7012eebd0dcf3fea
nerfedelmo <manansalapogi17@gmail.com>
2025-03-03 00:56:28 +0800

    rawr

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -18,20 +18,7 @@
 
 <body>
     <!-- Menu bar -->
-    <section id="menu">
-        <div class="logo">
-            <img src="" alt="Purrfectly Stitch">
-            <h2>Purrfectly Stitch</h2>
-        </div>
-        <div class="items">
-            <li><i class="bi bi-clipboard2-data-fill"></i><a href=""> Dashboard</a></li>
-            <li><i class="bi bi-file-earmark-text-fill"></i><a href=""> Reports</a></li>
-            <li><i class="bi bi-bag-check-fill"></i><a href=""> Purchase Order</a></li>
-            <li><i class="bi bi-people-fill"></i><a href=""> Users</a></li>
-            <li><i class="bi bi-box2-heart-fill"></i><a href=""> Products</a></li>
-
-        </div>
-    </section>
+    <?php include 'admin_menu.php'; ?>
 
     <!-- main display -->
     <section id="interface">
