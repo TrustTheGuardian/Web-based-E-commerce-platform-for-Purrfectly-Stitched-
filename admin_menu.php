42a2b508d9e41ba18e43d698b422cc2e63a919e9
nerfedelmo <manansalapogi17@gmail.com>
2025-03-03 01:13:53 +0800

    rawr

--- admin_menu.php
+++ admin_menu.php
@@ -4,10 +4,10 @@
         <h2>Purrfectly Stitch</h2>
     </div>
     <div class="items">
-        <li><i class="bi bi-clipboard2-data-fill"></i><a href="dashboard.php"> Dashboard</a></li>
-        <li><i class="bi bi-file-earmark-text-fill"></i><a href="reports.php"> Reports</a></li>
-        <li><i class="bi bi-bag-check-fill"></i><a href="purchase_order.php"> Purchase Order</a></li>
-        <li><i class="bi bi-people-fill"></i><a href="users.php"> Users</a></li>
-        <li><i class="bi bi-box2-heart-fill"></i><a href="products.php"> Products</a></li>
+        <li><i class="bi bi-clipboard2-data-fill"></i><a href="admin_dashboard.php"> Dashboard</a></li>
+        <li><i class="bi bi-file-earmark-text-fill"></i><a href="admin_reports.php"> Reports</a></li>
+        <li><i class="bi bi-bag-check-fill"></i><a href=""> Purchase Order</a></li>
+        <li><i class="bi bi-people-fill"></i><a href=""> Users</a></li>
+        <li><i class="bi bi-box2-heart-fill"></i><a href=""> Products</a></li>
     </div>
 </section>
